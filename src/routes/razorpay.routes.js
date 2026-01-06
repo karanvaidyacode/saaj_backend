49e0395d00f9e1273bd94427eadd3b0cd08b5416
karanvaidyacode <karanvaidyacode@gmail.com>
2026-01-06 12:55:14 +0530

    Use environment variables for Razorpay API keys
    Updated Razorpay configuration to use environment variables for API keys.

--- src/routes/razorpay.routes.js
+++ src/routes/razorpay.routes.js
@@ -8,8 +8,8 @@ const {
 const router = express.Router();
 
 const razorpay = new Razorpay({
-  key_id: "rzp_live_S0V9OfWCncAp7b", // Replace with your Key ID
-  key_secret: "AHFdhvbhjHwAwKZE2WbnwktG", // Replace with your Key Secret
+  key_id: process.env.RAZORPAY_API_KEY, // Replace with your Key ID
+  key_secret: process.env.RAZORPAY_API_SECRET, // Replace with your Key Secret
 });
 
 // Endpoint to create a new Razorpay order
